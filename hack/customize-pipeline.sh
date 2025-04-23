3bb9d879106da0eef7ff80e70069137f85c55892
Jan Fajerski <jfajersk@redhat.com>
2025-04-23 11:39:31 +0200

    add pipeline customization script
    This script is and should stay idempotent.
    
    Signed-off-by: Jan Fajerski <jfajersk@redhat.com>

---
+++ hack/customize-pipeline.sh
@@ -0,0 +1,11 @@
+branch="main"
+for file in "$@"
+do
+    action=$(basename `expr "$file" : '.*\(pu.*\.yaml\)'` .yaml | tr - _)
+    dockerfile=$(yq '.spec.params[] | select(.name == "dockerfile").value' "$file")
+    export trigger="event == \"$action\" && target_branch == \"$branch\" && (\"catalog\".pathChanged() || \"$dockerfile\".pathChanged())"
+    echo "Processing file $file"
+    yq -i '.metadata.annotations += {"pipelinesascode.tekton.dev/on-cel-expression": strenv(trigger)}' "$file"
+    yq -i '(.spec.params[] | select(.name == "build-platforms").value | select(length == 1)) += ["linux/arm64","linux/ppc64le","linux/s390x"]' "$file"
+    yq -i 'with(.spec.params; select(all_c(.name != "build-source-image")) | . += [{"name": "build-source-image", "value": "true"}])' "$file"
+done
