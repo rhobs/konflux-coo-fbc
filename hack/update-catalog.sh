1024b6d3bcd662a8719b8df28b596b1b20fd2bc9
Jenny Zhu <jenny.a.zhu@gmail.com>
2025-06-24 20:25:52 -0400

    [COO 1.2.1] Manually edit update-catalog.sh and catalog-template.yaml
    - update to latest cluster-observability-operator-bundle SHA from Jun 24, 2025, 2:07 PM ET
    - update to catalog-template.yaml to include COO 1.2.1 in the property 'entries'

--- hack/update-catalog.sh
+++ hack/update-catalog.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-LATEST="quay.io/redhat-user-workloads/cluster-observabilit-tenant/cluster-observability-operator/cluster-observability-operator-bundle@sha256:3509dccf7ace97a93f89e89f0afb15491028513453bf6a86cb532ea9add027ea"
+LATEST="quay.io/redhat-user-workloads/cluster-observabilit-tenant/cluster-observability-operator/cluster-observability-operator-bundle@sha256:53fb99ba28d89473bf4d884115a175eda7334adbcdc5ee9f635a7ab0fb01961f"
 
 VALUE="$LATEST" yq -i '.entries[-1].image=strenv(VALUE)' catalog/catalog-template.yaml
