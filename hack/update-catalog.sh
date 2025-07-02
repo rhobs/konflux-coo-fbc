250d2962c7908f60f5c2554ad22de5bb4e47b29b
Gabriel Bernal <gbernal@redhat.com>
2025-07-02 17:20:38 +0200

    update bundle to snapshot 5t29l

--- hack/update-catalog.sh
+++ hack/update-catalog.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-LATEST="quay.io/redhat-user-workloads/cluster-observabilit-tenant/cluster-observability-operator/cluster-observability-operator-bundle@sha256:872715105511f5767b69611f00ad4fc8c44e0855e3007dc303dc9b7871e64bfc"
+LATEST="quay.io/redhat-user-workloads/cluster-observabilit-tenant/cluster-observability-operator/cluster-observability-operator-bundle@sha256:e2822573b0842ed5b4252d598c538cceddcfd1ada340a30b8447427af321d80e"
 
 VALUE="$LATEST" yq -i '.entries[-1].image=strenv(VALUE)' catalog/catalog-template.yaml
