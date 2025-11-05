cf96c061f971f6726b628dfb4d7b72d2141ecf91
lihongyan1 <hongyli@redhat.com>
2025-11-05 01:03:40 -0500

    Update catalog with new bundle including fix of korrel8r

--- hack/update-catalog.sh
+++ hack/update-catalog.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-LATEST="quay.io/redhat-user-workloads/cluster-observabilit-tenant/cluster-observability-operator/cluster-observability-operator-bundle@sha256:cf6706b0bc9d2bfe444bcb6a9683b4752944947e6ca79b779aa6f66613d29b98"
+LATEST="quay.io/redhat-user-workloads/cluster-observabilit-tenant/cluster-observability-operator/cluster-observability-operator-bundle@sha256:e78fa58b90e2386796c51fa4c58f939b5924eb6eb85ba3d1a184f798dfb29991"
 
 VALUE="$LATEST" yq -i '.entries[-1].image=strenv(VALUE)' catalog/catalog-template.yaml
