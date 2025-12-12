e83ed0ba2160c306195345828e47600a1a149c82
lihongyan1 <hongyli@redhat.com>
2025-12-11 22:46:30 -0500

    Update catalog

--- hack/update-catalog.sh
+++ hack/update-catalog.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-LATEST="quay.io/redhat-user-workloads/cluster-observabilit-tenant/cluster-observability-operator/cluster-observability-operator-bundle@sha256:bff368bc64ad689e10e3139b96404999570cad3d52bf51a57d8c83063106ea47"
+LATEST="quay.io/redhat-user-workloads/cluster-observabilit-tenant/cluster-observability-operator/cluster-observability-operator-bundle@sha256:8f8970f5807e51590139f8d0d7882704147792fcec5f7f850380832ee57d1897"
 
 VALUE="$LATEST" yq -i '.entries[-1].image=strenv(VALUE)' catalog/catalog-template.yaml
