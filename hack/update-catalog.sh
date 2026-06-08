4d434b37c00f2d013589f1b745dbb4ada673cbd6
PeterYurkovich <pyurkovi@redhat.com>
2026-06-08 14:53:13 -0400

    feat: update bundle image

--- hack/update-catalog.sh
+++ hack/update-catalog.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-LATEST="quay.io/redhat-user-workloads/cluster-observabilit-tenant/cluster-observability-operator/cluster-observability-operator-bundle@sha256:af098588da3406f9cbf0109d8b9fff5e75b2f1f8df48887d5dc309cf9a2a21dc"
+LATEST="quay.io/redhat-user-workloads/cluster-observabilit-tenant/cluster-observability-operator/cluster-observability-operator-bundle@sha256:bc5d9a8b0faa568698aff896cf5210e924b4c9a2615331e9972071e4a6c4a5ab"
 
 VALUE="$LATEST" yq -i '.entries[-1].image=strenv(VALUE)' catalog/catalog-template.yaml
