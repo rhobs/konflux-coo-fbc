38ea90a0d15ac61393fdff830d38318f9f33a4e6
PeterYurkovich <pyurkovi@redhat.com>
2026-06-08 15:13:01 -0400

    chore(deps) update 1.5 bundle

--- hack/update-bundle-1.5.0.sh
+++ hack/update-bundle-1.5.0.sh
@@ -9,4 +9,4 @@
 #   The GitHub Actions workflow will regenerate catalogs automatically.
 
 BUNDLE_VERSION="1.5.0"
-LATEST="quay.io/redhat-user-workloads/cluster-observabilit-tenant/cluster-observability-operator/cluster-observability-operator-bundle@sha256:5aefb4e0fa0e9aff43536ca39586b3734f809790bed9fe796d09c357bf0555ce"
+LATEST="quay.io/redhat-user-workloads/cluster-observabilit-tenant/cluster-observability-operator/cluster-observability-operator-bundle@sha256:bc5d9a8b0faa568698aff896cf5210e924b4c9a2615331e9972071e4a6c4a5ab"
