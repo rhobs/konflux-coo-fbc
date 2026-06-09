49157b3152394b44b41950a702600559fbba2596
Jan Fajerski <jfajersk@redhat.com>
2026-06-09 09:52:00 +0200

    fix bundle reference nudging and annotating the generate PR
    Signed-off-by: Jan Fajerski <jfajersk@redhat.com>

--- hack/update-catalog.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-LATEST="quay.io/redhat-user-workloads/cluster-observabilit-tenant/cluster-observability-operator/cluster-observability-operator-bundle@sha256:af098588da3406f9cbf0109d8b9fff5e75b2f1f8df48887d5dc309cf9a2a21dc"
-
-VALUE="$LATEST" yq -i '.entries[-1].image=strenv(VALUE)' catalog/catalog-template.yaml
